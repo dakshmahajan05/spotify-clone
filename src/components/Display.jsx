1fc0468d50916eecf3959db7be2bcce959e91cb1
Daksh Mahajan <mahajandaksh682@gmail.com>
2025-10-03 01:05:00 +0530

    added bg color logic for albums

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-import { Route,Routes } from 'react-router-dom'
+import React, { useEffect, useRef } from 'react'
+import { Route,Routes, useLocation } from 'react-router-dom'
 import DisplayHome from './DisplayHome'
 import DisplayAlbum from './DisplayAlbum'
+import { albumsData } from '../assets/frontend-assets/assets'
 
 const display = () => {
+
+  const displayref = useRef();
+  const location =useLocation();
+
+  const isalbum = location.pathname.includes("album");
+  const albumId= isalbum?location.pathname.slice(-1):"";
+  const bgcolor = albumsData[Number(albumId)].bgColor
+  console.log(bgcolor);
+
+
+    useEffect(()=>{
+      if(isalbum){
+        displayref.current.style.background=`linear-gradient(${bgcolor},#12121212)`
+      }else{
+        displayref.current.style.background=`#12121212`
+
+      }
+    })
+  
+
   return (
-    <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0 '>
+    <div ref={displayref} className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0 '>
       <Routes>
         <Route path='/'element={<DisplayHome/>}/>
         <Route path='/album/:id' element={<DisplayAlbum/>}/>
