8c7429881b45f27caff192b2eee33473063bc124
Daksh Mahajan <mahajandaksh682@gmail.com>
2025-10-02 16:54:42 +0530

    created sidebar ui

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-=======
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 21bde941d018cb2df681d1f6cde2f046cf302b47
 })
